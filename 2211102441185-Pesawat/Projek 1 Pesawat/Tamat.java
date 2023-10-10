94a8676b2d89870507fb4f4f84df6092f8b9d653
SlJlN <2211102441185@umkt.ac.id>
2023-10-10 23:00:33 +0800

    Projek Shooter
    Muhammad Afif Aunur Rohman
    2211102441185

--- 2211102441185-Pesawat/Projek 1 Pesawat/Tamat.java
+++ 2211102441185-Pesawat/Projek 1 Pesawat/Tamat.java
@@ -1,19 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Characters here.
+ * Write a description of class Tamat here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Characters extends Actor
+public class Tamat extends Actor
 {
     /**
-     * Act - do whatever the Characters wants to do. This method is called whenever
+     * Act - do whatever the Tamat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
         // Add your action code here.
+        GreenfootImage image = new GreenfootImage("tamat",50,Color.BLACK, null);
+        setImage(image);
+        Greenfoot.stop();
+        
     }
 }
